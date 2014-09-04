36d08e5b7ec55cc38a1be801bab8f73ffc21f06c
Geonmo <ry840901@gmail.com>
2014-09-04 15:56:28 +0900

    Add zboson to ntuplizer.

---
+++ JpsiAnalyzer/ntuple_from_flatEDM_cfg.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Ntuple")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'file:out.root',
+    ),
+)
+
+process.load("KrAFT.GenericNtuple.flatNtuple_cfi")
+process.load("KrAFT.RecoSelectorTools.zSelector_cfi")
+process.load("KrAFT.GenericNtuple.flatZNtuple_cfi")
+process.passFEDM = cms.EDFilter("HLTHighLevel",
+    eventSetupPathsKey = cms.string(''),
+    TriggerResultsTag = cms.InputTag("TriggerResults","","KrAFT"),
+    HLTPaths = cms.vstring(
+        "CANDSEL",
+    ),
+    throw = cms.bool(False),
+    andOr = cms.bool(True)
+)
+
+#process.p = cms.Path(process.passFEDM+process.fEvent)
+process.p = cms.Path(process.ZToMuMuCandidates+process.ZToElElCandidates*process.fEvent)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("ntuple.root"),
+)
+
+setattr( process.fEvent.cands,"zMuMu",process.zMuMu)
+setattr( process.fEvent.cands,"zElEl",process.zElEl)
+
+
